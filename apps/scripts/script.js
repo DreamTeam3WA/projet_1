262f4c551024faa1b400f74882bce6e9d63bc51a
MikaelCabral <don_cabral@hotmail.fr>
2015-05-13 16:44:47 +0200

    Mise à jour

---
+++ apps/scripts/script.js
@@ -0,0 +1,9 @@
+$('.signin').click(function(){
+	$('.log').removeClass('notvisible');
+	$('.log').addClass('visible');
+})
+
+$('.signup').click(function(){
+	alert('blabla');
+	$('.notvisible').toogleClass('visible');
+})
