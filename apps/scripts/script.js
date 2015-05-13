dd2b85e99134027e32dd4e9317a7718d25fb778d
MikaelCabral <don_cabral@hotmail.fr>
2015-05-13 17:03:34 +0200

    mise a jour

--- apps/scripts/script.js
+++ apps/scripts/script.js
@@ -1,12 +1,17 @@
 $('document').ready(function()
 {
 	$('.header .signin').click(function(){
-		$('.notvisiblein').css('display','block');
-		$('.notvisiblelog').css('display','none');
+		$('.notvisiblein').css('display','none');
+		$('.notvisiblelog').css('display','block');
 	});
 
 	$('.header .signup').click(function(){
-		$('.notvisiblelog').css('display','block');
-		$('.notvisiblein').css('display','none');
+		$('.notvisiblelog').css('display','none');
+		$('.notvisiblein').css('display','block');
 	});
+
+	$('.fermer').click(function(){
+		$('.notvisiblein').css('display','none');
+		$('.notvisiblelog').css('display','none');
+	})
 });
