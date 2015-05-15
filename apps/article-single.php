37a79807829544d6db4f5a7081f9fce8e206c484
Doppelganger217 <alex.ctx217@gmail.com>
2015-05-15 16:36:52 +0200

    Alex:

--- apps/article-single.php
+++ apps/article-single.php
@@ -5,11 +5,7 @@ $db->exec("SET CHARACTER SET utf8");
 $tab = $db->query("SELECT * FROM articles")->fetchAll(PDO::FETCH_ASSOC);
 
 $i=0;
-<<<<<<< HEAD
-while ($i<4){
-=======
 while ($i<3){
->>>>>>> 21cc5c77505d357ed28112de02a5ba5f773f561c
 	$titre = $tab[$i]['titre'];
 	$date = $tab[$i]['date'];
 	$user = $tab[$i]['user'];
