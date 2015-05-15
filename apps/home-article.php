c026973fe6df4c29073a7a3bcf4715a698f7e696
h-laurent <laurent.hansmaennel@gmail.com>
2015-05-15 16:53:56 +0200

    maj
    Signed-off-by: h-laurent <laurent.hansmaennel@gmail.com>

--- apps/home-article.php
+++ apps/home-article.php
@@ -2,16 +2,16 @@
 
 $db = new PDO("mysql:dbname=filrouge;host=127.0.0.1", 'root', 'troiswa');
 $db->exec("SET CHARACTER SET utf8");
-$tab = $db->query("SELECT * FROM articles")->fetchAll(PDO::FETCH_ASSOC);
+$tab = $db->query("SELECT * FROM articles ORDER BY id DESC")->fetchAll(PDO::FETCH_ASSOC);
 
 $i=0;
-while ($i<3){
+while ($i<2){
 	$titre = $tab[$i]['titre'];
 	$date = $tab[$i]['date'];
 	$user = $tab[$i]['user'];
 	$lien = $tab[$i]['lien'];
 	$description = $tab[$i]['description'];
-	require('views/article-single.phtml');
+	require('views/home-article.phtml');
 	$i++;
 }
 
