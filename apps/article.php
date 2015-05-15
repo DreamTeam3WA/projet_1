8a20a8375dad0627b2dc1fd755be8d82c730258a
h-laurent <laurent.hansmaennel@gmail.com>
2015-05-15 14:57:34 +0200

    maj

--- apps/article.php
+++ apps/article.php
@@ -1,12 +1,5 @@
-<?php
-if (($handle = fopen("./models/article.csv", "r")) !== FALSE) {
-    while (($data = fgetcsv($handle, 0, ";")) !== FALSE) {
-           $date = $data[0];
-           $h2 = $data[1];
-          $description = $data[2];
-          $source = $data[3];
-			require('views/article.phtml');
-    }
-    fclose($handle);
-}
+ <?php
+ 
+require('views/article.phtml');
 ?>
+
