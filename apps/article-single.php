5c5da74122da0892b52524b887375033222331f0
Doppelganger217 <alex.ctx217@gmail.com>
2015-05-15 14:02:42 +0200

    Alex:

---
+++ apps/article-single.php
@@ -0,0 +1,18 @@
+ <?php
+
+$db = new PDO("mysql:dbname=filrouge;host=127.0.0.1", 'root', 'troiswa');
+$tab = $db->query("SELECT * FROM articles")->fetchAll(PDO::FETCH_ASSOC);
+var_dump($tab);
+
+$i=0;
+while ($i<4){
+	$titre = $tab[$i]['titre'];
+	$date = $tab[$i]['date'];
+	$user = $tab[$i]['user'];
+	$lien = $tab[$i]['lien'];
+	$description = $tab[$i]['description'];
+	require('views/article-single.phtml');
+	$i++;
+}
+
+?>
