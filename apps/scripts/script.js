0f8ac4d8067229b32e5a015c9d4632b7398b34ea
MikaelCabral <don_cabral@hotmail.fr>
2015-05-15 09:24:58 +0200

    Mise a jour

--- apps/scripts/script.js
+++ apps/scripts/script.js
@@ -4,20 +4,27 @@ $('document').ready(function()
 {
 	$('.header .signup, .inscr .fermer').click(function(){
 		 $( ".inscr" ).toggle(500);
+		 $('.log').css('display','none');
+		 $('.article').css('display','none');
+
 	});
 
 	$('.header .signin, .log .fermer').click(function(){
 		 $( ".log" ).toggle(500);
+		 $('.inscr').css('display','none');
+		 $('.article').css('display','none');
 	});
+
+	$('.header .newarticle, .article .fermer').click(function(){
+		$( ".article" ).toggle(500);
+		 $('.log').css('display','none');
+		 $('.inscr').css('display','none');
+	})
 
 	$('.tchat h3').click(function(){
 		 $( "div.tchat-wd" ).toggle( 500);
 	});
-	$('.header .newarticle').click(function(){
-		$('.notvisibleart').css('display','block');
-		$('.notvisiblein').css('display','none');
-		$('.notvisiblelog').css('display','none');
-	});
+
 	$('.navforum .newsujet').click(function(){
 		$('.forum').css('display','none');
 		$('.sujetforum').css('display','none');
