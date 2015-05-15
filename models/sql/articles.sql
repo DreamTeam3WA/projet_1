c0c3d06b134a0d7ad4bb1c2641999f8ed8859a91
MikaelCabral <don_cabral@hotmail.fr>
2015-05-15 11:59:47 +0200

    Mise a jour mika

--- models/sql/articles.sql
+++ models/sql/articles.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client: localhost
--- Généré le: Ven 15 Mai 2015 à 11:56
+-- Généré le: Ven 15 Mai 2015 à 11:59
 -- Version du serveur: 5.5.41-0ubuntu0.14.04.1
 -- Version de PHP: 5.5.9-1ubuntu4.7
 
@@ -35,28 +35,6 @@ CREATE TABLE IF NOT EXISTS `articles` (
   `description` varchar(1024) COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `user`
---
-
-CREATE TABLE IF NOT EXISTS `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `login` varchar(32) COLLATE utf8_bin NOT NULL,
-  `password` varchar(512) COLLATE utf8_bin NOT NULL,
-  `email` varchar(128) COLLATE utf8_bin NOT NULL,
-  `date_register` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=2 ;
-
---
--- Contenu de la table `user`
---
-
-INSERT INTO `user` (`id`, `login`, `password`, `email`, `date_register`) VALUES
-(1, 'mikiletoss', 'mdp', 'don_cabral@hotmail.fr', '2015-05-15 09:21:07');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
