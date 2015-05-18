6d93e1a914913f0277d45855663a41d86d764c32
h-laurent <laurent.hansmaennel@gmail.com>
2015-05-18 11:48:13 +0200

    nmaj

--- apps/test.php
+++ apps/test.php
@@ -1,6 +1,4 @@
 <?php
-$db = new PDO("mysql:dbname=filrouge;host=127.0.0.1", 'root', 'troiswa');
-$tab = $db->query("SELECT * FROM user")->fetchAll(PDO::FETCH_ASSOC);
 var_dump($tab);
 var_dump($tab[0]['login']);
 ?>
--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php
+	$db = new PDO("mysql:dbname=filrouge;host=127.0.0.1", 'root', 'troiswa');
+	$db->exec("SET CHARACTER SET utf8");
+	
 	$page = 'home';
 	if (isset($_GET['page']))
 		$page = $_GET['page'];
 	require('./views/skel.phtml');
+	if (!isset($user)){
+		$user = 'none';
+	}
 ?>
