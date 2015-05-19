962eb993b8f3c25b2de70cfc28d33307ae6be7f9
MikaelCabral <don_cabral@hotmail.fr>
2015-05-19 10:01:40 +0200

    mise a jour

---
+++ apps/livredorfooter.php
@@ -0,0 +1,6 @@
+<?php
+$tab = $db->query("SELECT * FROM livredor ORDER BY date DESC")->fetchAll(PDO::FETCH_ASSOC);
+$titre = $tab[0]['titre'];
+$description = $tab[0]['description'];
+require('views/livredorfooter.phtml');
+?>
