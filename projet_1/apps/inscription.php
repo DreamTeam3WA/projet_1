a6937424fbd90fc59682b0c3d1f58ed6e8fe3c3f
h-laurent <laurent.hansmaennel@gmail.com>
2015-05-19 13:31:25 +0200

    maj

---
+++ projet_1/apps/inscription.php
@@ -0,0 +1,21 @@
+<?php 
+
+if ($_POST['password'] === $_POST['password2'])
+{
+	$req = "SET ";
+	foreach($_POST as $key => $val){
+		$_POST[$key] = $db -> quote($val);
+		$req.= $key."=".$_POST[$key].", ";
+	}
+
+	$req = substr($req,0,-2);
+	var_dump($req);
+	$db-> exec("INSERT INTO user $req");
+
+	require('views/inscription-ok.phtml');
+}
+else {
+	require('views/inscription-nok.phtml');
+}
+
+?>
