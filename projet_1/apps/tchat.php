8d51ac0100d264ae350bbcfae36a69aa0d63cc51
Doppelganger217 <alex.ctx217@gmail.com>
2015-05-20 16:07:21 +0200

    alex

--- projet_1/apps/tchat.php
+++ projet_1/apps/tchat.php
@@ -1,39 +1,9 @@
  <?php
-
-
-
-if (isset($_POST) && isset($_POST['tchat']))
+ 
+if (isset($_POST) && isset($_POST['tchat']) && !empty($_POST['tchat']))
 {
-/*
-	$req = "";
-	foreach($_POST as $key => $val)
-	{
-		$_POST[$key] = $db->quote($val);
-		$req .= $key."=".$_POST[$key].", ";
-	}
-	$req = substr($req,0,-2);
-*/
-	// var_dump($req);
-	
 	$db->exec('INSERT INTO tchat SET message='.$db->quote($_POST['tchat']));
-	// $db->exec("INSERT INTO tchat SET $req");
+
 }
-
-$tab = $db->query("SELECT * FROM tchat")->fetchAll(PDO::FETCH_ASSOC);
-
-
-$i=0;
-while ($i<count($tab)){
-	$message = $tab[$i]['message'];
-		if($i%2 == 0){
-			$class='tchat-pair';
-		}
-		else{
-			$class='tchat-impair';
-		}
 	require('views/tchat.phtml');
-	$i++;
-}
-
-
 ?>
