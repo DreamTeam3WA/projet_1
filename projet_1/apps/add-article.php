d74288a48c092ad262e78ccb2c7c6c9815f7ebb7
Doppelganger217 <alex.ctx217@gmail.com>
2015-05-20 09:46:28 +0200

    alex

--- projet_1/apps/add-article.php
+++ projet_1/apps/add-article.php
@@ -9,7 +9,7 @@ if (isset($_POST))
 		$req .= $key."=".$_POST[$key].", ";
 	}
 	$req = substr($req,0,-2);
-	var_dump($req);
+
 	echo $db->exec("INSERT INTO articles SET $req");
 }
 	require('./views/add-article.phtml');
