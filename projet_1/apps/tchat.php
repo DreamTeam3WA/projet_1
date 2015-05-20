4de7be8b5096d3a2d4dc00245b3fffe64083833d
p-steve <pflaum.steve67@gmail.com>
2015-05-20 16:46:22 +0200

    steve

--- projet_1/apps/tchat.php
+++ projet_1/apps/tchat.php
@@ -4,19 +4,10 @@
 
 if (isset($_POST) && isset($_POST['tchat']))
 {
-/*
-	$req = "";
-	foreach($_POST as $key => $val)
-	{
-		$_POST[$key] = $db->quote($val);
-		$req .= $key."=".$_POST[$key].", ";
-	}
-	$req = substr($req,0,-2);
-*/
-	// var_dump($req);
+
 	
 	$db->exec('INSERT INTO tchat SET message='.$db->quote($_POST['tchat']));
-	// $db->exec("INSERT INTO tchat SET $req");
+
 }
 
 $tab = $db->query("SELECT * FROM tchat")->fetchAll(PDO::FETCH_ASSOC);
