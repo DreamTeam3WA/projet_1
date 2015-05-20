c9c0d298d5b714ab86f2ffc2a65129a3f26b7d17
p-steve <pflaum.steve67@gmail.com>
2015-05-20 15:20:41 +0200

    steve trop cool !

--- projet_1/apps/header-log.php
+++ projet_1/apps/header-log.php
@@ -4,18 +4,13 @@ if($_SESSION!= null){
 	$id = $_SESSION['id'];
 	$tab = $db->query("SELECT * FROM user WHERE id = '$id'")->fetchAll(PDO::FETCH_ASSOC);
 	$user = $tab[0]['login'];
-<<<<<<< HEAD
-
-	require('views/header-log-in.phtml');
-=======
-	if($tab[0]['droits'] == 1)
+	if(isset($tab[0]['droits']) && $tab[0]['droits'] == 1)
 	{
 		require('views/header-admin.phtml');
 	}
 	else {
 		require('views/header-user.phtml');
 	}
->>>>>>> 640a34326868e3fd7120c2f84de541250bad248c
 }
 
 else {
--- projet_1/apps/login.php
+++ projet_1/apps/login.php
@@ -2,18 +2,9 @@
 
 $login = $_POST;
 $user = $login['user'];
-$pass = $login[pass];
-
-<<<<<<< HEAD
-$_SESSION['id'] = 1;
-
-
-// echo $pass;
+$pass = $login["pass"];
 
 $tab = $db->query("SELECT * FROM user WHERE login = ".$db->quote($user))->fetchAll(PDO::FETCH_ASSOC);
-=======
-$tab = $db->query("SELECT * FROM user WHERE login = '$user'")->fetchAll(PDO::FETCH_ASSOC);
->>>>>>> 640a34326868e3fd7120c2f84de541250bad248c
 
 if ($tab !=null){
 	if ($tab[0]['password'] == $pass){
