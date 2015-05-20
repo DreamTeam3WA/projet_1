72583c4726c7864c90b2e59f5eb3536aa74d9fc5
Doppelganger217 <alex.ctx217@gmail.com>
2015-05-20 11:46:49 +0200

    alex

--- projet_1/apps/inscription.php
+++ projet_1/apps/inscription.php
@@ -1,24 +1,26 @@
 <?php 
-
-if ($_POST['password'] === $_POST['password2'])
-{
-	$req = "SET ";
-	foreach($_POST as $key => $val){
-		if($key!="password2"){
-			$_POST[$key] = $db -> quote($val);
-			$req.= $key."=".$_POST[$key].", ";
+if (isset($_POST) && isset($_POST['password2'])){
+	if ($_POST['password'] === $_POST['password2'])
+	{
+		$req = "SET ";
+		foreach($_POST as $key => $val){
+			if($key!="password2"){
+				$_POST[$key] = $db -> quote($val);
+				$req.= $key."=".$_POST[$key].", ";
+			}
 		}
+
+		$req = substr($req,0,-2);
+
+
+		$db-> exec("INSERT INTO user $req");
+		
+		require('views/inscription-ok.phtml');
 	}
-
-	$req = substr($req,0,-2);
-
-
-	$db-> exec("INSERT INTO user $req");
-	
-	require('views/inscription-ok.phtml');
+	else {
+		require('views/inscription-nok.phtml');
+	}
 }
-else {
-	require('views/inscription-nok.phtml');
-}
+require('./views/inscription.phtml');
 
 ?>
