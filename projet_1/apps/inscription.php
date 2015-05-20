55556b6f4673055ff3065f9f4c7ecee26db0fbd3
Doppelganger217 <alex.ctx217@gmail.com>
2015-05-20 11:54:19 +0200

    alex

--- projet_1/apps/inscription.php
+++ projet_1/apps/inscription.php
@@ -1,10 +1,13 @@
 <?php 
-if (isset($_POST) && isset($_POST['password2'])){
+if (isset($_POST) && isset($_POST['action']) && $_POST['action'] == "register"){
 	if ($_POST['password'] === $_POST['password2'])
 	{
 		$req = "SET ";
+
+		$exclude_key = array("password2", "action");
+ 
 		foreach($_POST as $key => $val){
-			if($key!="password2"){
+			if(!in_array($key, $exclude_key)){
 				$_POST[$key] = $db -> quote($val);
 				$req.= $key."=".$_POST[$key].", ";
 			}
