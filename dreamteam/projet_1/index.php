1d95576604cd1951ca6f5a988bf999e843ab3c07
Doppelganger217 <alex.ctx217@gmail.com>
2015-05-22 16:22:09 +0200

    alex

--- dreamteam/projet_1/index.php
+++ dreamteam/projet_1/index.php
@@ -4,17 +4,25 @@
 	$db = new PDO("mysql:dbname=filrouge;host=127.0.0.1", 'root', 'troiswa');
 	$db->exec("SET CHARACTER SET utf8");
 	
+if (isset($_GET['ajax']))
+{
+	$page = $_GET['ajax'];
+	require('apps/'.$page.'.php');
+}
+else
+{
+
 	if (isset($_GET['articleid'])){
 		$articleid = $_GET['articleid'];
 	}
 	if (isset($_GET['sujetid'])){
 		$sujetid = $_GET['sujetid'];
 	}
-
 	$page = 'home';
 	if (isset($_GET['page'])){
 		$page = $_GET['page'];
 	}
 
 	require('./views/skel.phtml');
+}
 ?>
