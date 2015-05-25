35fe003ae437e11321e9bd4a235230d83c4c7bb0
Doppelganger217 <alex.ctx217@gmail.com>
2015-05-25 09:42:51 +0200

    dsf

--- dreamteam/projet_1/apps/tchat-form.php
+++ dreamteam/projet_1/apps/tchat-form.php
@@ -2,7 +2,8 @@
  
 if (isset($_POST) && isset($_POST['message']) && !empty($_POST['message']))
 {
-	$db->exec('INSERT INTO tchat SET message='.$db->quote($_POST['message']));
+	
+	$db->exec('INSERT INTO tchat SET message='.$message = $db->quote($_POST['message']).', id_user='.$message = $db->quote($_POST['$id_user']));
 
 }
 	
--- dreamteam/projet_1/apps/tchat.php
+++ dreamteam/projet_1/apps/tchat.php
@@ -1,3 +1,9 @@
  <?php
+ if($_SESSION!= null){
+	$id_user = $_SESSION['id'];
+
+	$tab = $db->query("SELECT * FROM user WHERE id = '$id_user'")->fetchAll(PDO::FETCH_ASSOC);
+	$user = $tab[0]['login'];
 	require('views/tchat.phtml');
+}
 ?>
