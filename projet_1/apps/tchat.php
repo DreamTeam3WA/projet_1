a05726db233173fc83c7eeeb3db070699c5c38b3
p-steve <pflaum.steve67@gmail.com>
2015-05-25 13:03:54 +0200

    recup stevuyyyyyyyyyyyyyyyyyy

--- projet_1/apps/tchat.php
+++ projet_1/apps/tchat.php
@@ -1,14 +1,7 @@
  <?php
-<<<<<<< HEAD:projet_1/apps/tchat.php
- 
-if (isset($_POST) && isset($_POST['tchat']) && !empty($_POST['tchat']))
-{
 
-	$db->exec('INSERT INTO tchat SET message='.$db->quote($_POST['tchat']));
-=======
 if($_SESSION!= null){
 	$id_user = $_SESSION['id'];
->>>>>>> 5c118b441cee25585f4984c2a5f6b82aa61d167c:dreamteam/projet_1/apps/tchat.php
 
 	$tab = $db->query("SELECT * FROM user WHERE id = '$id_user'")->fetchAll(PDO::FETCH_ASSOC);
 	echo $user;
