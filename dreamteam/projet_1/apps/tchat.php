5c118b441cee25585f4984c2a5f6b82aa61d167c
Doppelganger217 <alex.ctx217@gmail.com>
2015-05-25 09:47:53 +0200

    alex

--- dreamteam/projet_1/apps/tchat.php
+++ dreamteam/projet_1/apps/tchat.php
@@ -3,11 +3,9 @@ if($_SESSION!= null){
 	$id_user = $_SESSION['id'];
 
 	$tab = $db->query("SELECT * FROM user WHERE id = '$id_user'")->fetchAll(PDO::FETCH_ASSOC);
+	echo $user;
 	$user = $tab[0]['login'];
-	if(isset($tab[0]['droits']) && ($tab[0]['droits'] == 1 || $tab[0]['droits'] == 2))
-	{
-		require('views/tchat.phtml');
-	}
+	require('views/tchat.phtml');
 }
 else {
 	require('views/tchat-logout.phtml');
