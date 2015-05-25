c16650fed3010d4ea301fb0d4c8985b95d32c615
p-steve <pflaum.steve67@gmail.com>
2015-05-25 09:50:35 +0200

    steve

--- dreamteam/projet_1/apps/article-suppr.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-	$db->exec("DELETE FROM articles WHERE id='$id_article'");
-
-	//require('./views/article-suppr.phtml');
-?>
--- dreamteam/projet_1/apps/forum.php
+++
@@ -1,8 +0,0 @@
-<?php
-	if(isset($id_sujet)){
-		require('apps/forum-sujet.php');
-	}
-	else {
-		require('views/forum-base.phtml');
-	}
-?>
