09cf6debd9ad5240b8453029b6669fd37d6a2ed4
MikaelCabral <don_cabral@hotmail.fr>
2015-05-27 13:23:56 +0200

    changement repertoire

--- projet_1/apps/tchat.php
+++ projet_1/apps/tchat.php
@@ -1,10 +1,13 @@
  <?php
- 
-if (isset($_POST) && isset($_POST['tchat']) && !empty($_POST['tchat']))
-{
-
-	$db->exec('INSERT INTO tchat SET message='.$db->quote($_POST['tchat']));
-
-}
+/** Pascal : $_SESSION ne sera jamais égal a null, mais plutôt a un tableau vide **/
+if($_SESSION!= null){
+	// $tab = $db->query("SELECT * FROM user WHERE id = '$id_user'")->fetchAll(PDO::FETCH_ASSOC);
+	// echo $user;
+	// $user = $tab[0]['login'];
 	require('views/tchat.phtml');
+}
+else {
+	require('views/tchat-logout.phtml');
+}
+	
 ?>
