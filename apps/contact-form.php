540a6df9043880f9725e0e599198c3c74f5a1844
p-steve <pflaum.steve67@gmail.com>
2015-05-27 15:41:26 +0200

    steve maj

--- apps/contact-form.php
+++ apps/contact-form.php
@@ -1,20 +1,35 @@
 <?php
-
-if (isset($_POST) && isset($_POST['message']) && !empty($_POST['message']))
-{
-	$req = "";
-	foreach($_POST as $key => $val)
-	{
-		$_POST[$key] = $db->quote($val);
-		$req .= $key."=".$_POST[$key].", ";
-	}
-
-	$req = substr($req,0,-2);
-	var_dump($req);
-	$db->exec("INSERT INTO contact SET $req");
-	require('./views/contact-ok.phtml');
+// var_dump($_POST, $_GET);
+if (!empty($_POST["email"]) && !empty($_POST["nom"]) && !empty($_POST["prenom"]) && !empty($_POST["objet"]) && !empty($_POST["message"]))
+{	
+	$emailcontact= $db->quote($_POST["email"]);
+	$nomcontact= $db->quote($_POST["nom"]);
+	$prenomcontact= $db->quote($_POST["prenom"]);
+	$objetcontact= $db->quote($_POST["objet"]);
+	$messagecontact= $db->quote($_POST["message"]);
+	$db->exec("INSERT INTO contact SET email=".$emailcontact.", nom=".$nomcontact.",  prenom=".$prenomcontact.", objet=".$objetcontact.", message=".$messagecontact);
+	// echo "INSERT INTO contact SET email=".$emailcontact.", nom=".$nomcontact.",  prenom=".$prenomcontact.", objet=".$objetcontact.", message=".$messagecontact;
 }
-else{
+
+
+// $email => "toto"
+// quote()
+// $email => "'toto'"
+
+
+// "INSERT INTO contact SET email=".$emailcontact.", nom=".$nomcontact.", prenom=".
+
+/*
+var prenom = "toto";
+var nom = "titi";
+document.write(prenom+" "+nom);
+toto titi
+*/
+
+
+
+
+
 	require('./views/contact.phtml');
-}
+
 ?>
--- apps/livredor.php
+++ apps/livredor.php
@@ -1,18 +1,11 @@
 <?php
-/** Pascal : $_POST existera toujours et sera "au pire" un tableau vide **/
-if (isset($_POST))
-{
-	$req = "";
-	foreach($_POST as $key => $val)
-	{
-		$_POST[$key] = $db->quote($val);
-		$req .= $key."=".$_POST[$key].", ";
-	}
-
-	$req = substr($req,0,-2);
-	// var_dump($req);
-	/** Pascal : Pas du tout bien niveau sécurité :o **/
-	$db->exec("INSERT INTO livredor SET $req");
+if (!empty($_POST["nomldo"]) && !empty($_POST["titreldo"]) && !empty($_POST["descriptionldo"]))
+{	
+	$nomldo= $db->quote($_POST["nomldo"]);
+	$titreldo= $db->quote($_POST["titreldo"]);
+	$descriptionldo= $db->quote($_POST["descriptionldo"]);
+	
+	$db->exec("INSERT INTO livredor SET nom=".$nomldo.", titre=".$titreldo.", description=".$descriptionldo);
 }
 	require('./views/livredor.phtml');
 ?>
